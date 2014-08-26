981d1dbba30cc615309ad0093bd7d1281ba67c87
joshsalvi <joshsalvi@jds-mbp.rockefeller.edu>
2014-08-26 18:21:53 -0400

    bug fix

--- mutualinformation2.m
+++ mutualinformation2.m
@@ -14,6 +14,13 @@ function I = mutualinformation2(x,y)
 % Joshua D Salvi, jsalvi@rockefeller.edu
 %
 
+if iscolumn(x) == 0
+    x = x';
+end
+if iscolumn(y) == 0
+    y = y';
+end
+
 % Freedman-Diaconis rule to calculate bin size
 bwx = 2*iqr(x)/length(x)^(1/3);
 nbx = ceil((max(x) - min(x))/bwx);
@@ -22,13 +29,17 @@ nby = ceil((max(y) - min(y))/bwy);
 
 % 2D histogram
 [nh,co] = hist3([x y],[nbx nby]);
-nh = nh./sum(nh);
+nh = nh./sum(sum(nh));
+nhlog = log2(nh);
+nhlog(nhlog==inf | nhlog==-inf)=0;
 
 % Marginal probabilities
 Inh1 = log2(sum(nh,1));
 Inh2 = log2(sum(nh,2));
+Inh1(Inh1==inf | Inh1==-inf)=0;
+Inh2(Inh2==inf | Inh2==-inf)=0;
 
 % Mutual information from the joint and marginal probabilities
-I = sum(sum(nh.*bsxfun(@minus,bsxfun(@minus,log2(nh),Inh1),Inh2)));
+I = sum(sum(nh.*bsxfun(@minus,bsxfun(@minus,nhlog,Inh1),Inh2)));
 
 end
